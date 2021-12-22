ecf8a7b42626a8f5dc7a4ae79d11c442a7e43b2d
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-12-22 18:13:52 +0100

    starting to sort array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { API_URL_POPULAR } from "utils/urls";
 import Header from "components/Header";
+import SortSelect from "components/SortSelect";
 import FirstPage from "components/FirstPage";
 import MoviePage from "components/MoviePage";
 
@@ -17,6 +18,7 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      {/* <SortSelect /> */}
       <Routes>
         <Route path="/" element={<FirstPage movies={firstPage} />} />
         <Route path="/moviepage/:id" element={<MoviePage />} />
--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const FirstPage = ({ movies }) => {
+  const [movieList, setMovieList] = useState(movies);
+
+  const sortByTitle = () => {
+    const sorted = [...movieList].sort((a, b) => {
+      return a.title.localeCompare(b.title);
+    });
+    setMovieList(sorted);
+  };
+
+  // const [movie, setMovie] = useState([]);
+  // const [sortMovies, setSortMovies] = useState("alphabetically");
+
+  // useEffect(() => {
+  //   const sortedMovies = (type) => {
+  //     const types = {
+  //       vote_average: "vote_average",
+  //       release_date: "release_date",
+  //       origina_title: "original_title",
+  //     };
+  //     const sortProperty = types[type];
+  //     const sorted = [...movies].sort(
+  //       (a, b) => b[sortProperty] - a[sortProperty]
+  //     );
+  //     console.log(sorted);
+  //     setMovie(sorted);
+  //   };
+
+  //   sortedMovies(sortMovies);
+  // }, [sortMovies]);
+
+  // const sortTitle = () => {
+  //   movies.sort((a, b) => a.title.localeCompare(b.title));
+  // };
+
   return (
     <section>
-      {movies.map((movie) => (
-        <Link key={movie.id} to={`/moviepage/${movie.id}`}>
-          <h1>{movie.original_title}</h1>
-          <img
-            src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-            alt={movie.title}
-          />
-        </Link>
-      ))}
+      <select onChange={(e) => e.target.value}>
+        <option value="vote_average">imdb rating</option>
+        <option value="release_date">release date</option>
+        <option value="original_title">alpabetically</option>
+      </select>
+      <button onClick={sortByTitle}>Sort by Title</button>
+
+      {movies
+        // .sort((a, b) => a.title.localeCompare(b.title))
+        .map((movie) => (
+          <Link key={movie.id} to={`/moviepage/${movie.id}`}>
+            <h1>{movie.original_title}</h1>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </Link>
+        ))}
     </section>
   );
 };
